dd6d4f40f9f6c280becc5f2a049442a4d50c909f
Toby <5722599@gmail.com>
2023-08-29 19:24:15 -0400

    friend component done

--- friends-list/src/Friend.jsx
+++ friends-list/src/Friend.jsx
@@ -1,8 +1,12 @@
-const Friend = () => {
+const Friend = ({ ...friend }) => {
 
   return (
     <>
-      Friend component
+      <img src={friend.image} />
+      <div>
+        <h4>{friend.name}</h4>
+        <div style={{ color: '#7e7e7e' }}>{friend.age} years</div>
+      </div>
     </>
   )
 }
