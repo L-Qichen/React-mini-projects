8a6599fe598b96583ab6ef911e26180b8049ce61
Toby <5722599@gmail.com>
2023-09-09 13:14:35 -0400

    Fixed style bug. tours mini project done.

--- tours/src/Tour.jsx
+++ tours/src/Tour.jsx
@@ -10,10 +10,12 @@ export const Tour = ({ tour, deleteTour }) => {
       <img src={image} alt={name} />
       <span className="price">${price}</span>
       <h3 className="tourName">{name}</h3>
-      <p className="info">{readMore ? info : info.substring(0, 400)}</p>
-      <span className="read-more" onClick={() => setReadMore(!readMore)}>
-        {readMore ? 'Read Less' : 'Read More'}
-      </span>
+      <p className="info">
+        {readMore ? info : `${info.substring(0, 300)}...`}
+        <span className="read-more" onClick={() => setReadMore(!readMore)}>
+          {readMore ? 'Read Less' : 'Read More'}
+        </span>
+      </p>
       <div className="btn-block">
         <button type="button" className="btn" onClick={() => deleteTour(tour.id)}>Not Interested</button>
       </div>
