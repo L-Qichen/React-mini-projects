0cacfeefefd2c284e3018cad7f88d522219c75c0
Toby <5722599@gmail.com>
2023-09-09 15:06:53 -0400

    Added data and finished the html part.

--- reviews/src/App.jsx
+++ reviews/src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from 'react'
+import data from "./data"
 import './style.css'
 
 function App() {
+  const [index, setIndex] = useState(0);
+  const { id, image, job, name, text } = data[index];
 
   return (
     <>
-      <div>reviews</div>
+      <div>
+        <h1>Reviews</h1>
+        <img src={image} alt={name} />
+        <h3>{name}</h3>
+        <h5>{job}</h5>
+        <p>{text}</p>
+        <div>
+          <button>left</button>
+          <button>right</button>
+        </div>
+        <button>surprise me</button>
+      </div>
     </>
   )
 }
