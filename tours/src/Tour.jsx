24565c776e755adfeb82025b56ac4641b6d4aaa1
Toby <5722599@gmail.com>
2023-09-09 13:08:04 -0400

    Added 'read more' feature

--- tours/src/Tour.jsx
+++ tours/src/Tour.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react'
+
 export const Tour = ({ tour, deleteTour }) => {
   const { image, info, name, price } = tour;
+
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <>
       <img src={image} alt={name} />
       <span className="price">${price}</span>
       <h3 className="tourName">{name}</h3>
-      <p className="info">{info}</p>
+      <p className="info">{readMore ? info : info.substring(0, 400)}</p>
+      <span className="read-more" onClick={() => setReadMore(!readMore)}>
+        {readMore ? 'Read Less' : 'Read More'}
+      </span>
       <div className="btn-block">
         <button type="button" className="btn" onClick={() => deleteTour(tour.id)}>Not Interested</button>
       </div>
