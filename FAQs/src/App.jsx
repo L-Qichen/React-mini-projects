3743cb7cbeb5ad9230b15a049738332b7812b6a7
Toby <5722599@gmail.com>
2023-09-21 14:58:30 -0400

    imported data and stored in questions state

--- FAQs/src/App.jsx
+++ FAQs/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
+import data from './data'
 import List from './List'
 
 import './style.css'
 
 function App() {
+  const [questions, setQuestions] = useState(data);
+  console.log(questions);
 
   return (
     <>
-      <List />
+      <List questions={questions} />
     </>
   )
 }
