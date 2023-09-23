5b82f9d19d706d59527268bfd337fb39c8c41a86
Toby <5722599@gmail.com>
2023-09-23 18:33:59 -0400

    set up the menu data as a state variable

--- menu/src/App.jsx
+++ menu/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import Title from './Title'
+import data from './data'
 import './style.css'
 
 function App() {
+  const [menu, setMenu] = useState(data);
+  console.log(menu);
 
   return (
     <article className='menu'>
