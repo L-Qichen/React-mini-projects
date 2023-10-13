09a12097a3bedb92f8ad594bd5d5d694e8f9e02a
Toby <5722599@gmail.com>
2023-10-12 21:18:51 -0400

    added some basic style

--- menu/src/MenuItem.jsx
+++ menu/src/MenuItem.jsx
@@ -2,11 +2,11 @@ export const MenuItem = ({ title, category, price, img, desc }) => {
 
   return (
     <>
-      <div>
+      <div className="menu-item">
+        <img className="img" src={img} alt={title} />
         <div>{title}</div>
         <div>{category}</div>
         <div>{price}</div>
-        <img src={img} alt={title} />
         <div>{desc}</div>
       </div>
     </>
