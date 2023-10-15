7af51cfbf12f9fd4029672a5d90cec3c572b9c64
Toby <5722599@gmail.com>
2023-10-15 18:23:33 -0400

    Finished style. Menu mini-project done.

--- menu/src/MenuItem.jsx
+++ menu/src/MenuItem.jsx
@@ -4,10 +4,13 @@ export const MenuItem = ({ title, category, price, img, desc }) => {
     <>
       <div className="menu-item">
         <img className="img" src={img} alt={title} />
-        <div>{title}</div>
-        <div>{category}</div>
-        <div>{price}</div>
-        <div>{desc}</div>
+        <div className="item-info">
+          <div className="title">
+            <h3>{title}</h3>
+            <div className="item-price">{price}</div>
+          </div>
+          <div className="item-text">{desc}</div>
+        </div>
       </div>
     </>
   );
