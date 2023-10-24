717be384a96489a04a0d0c1cc36680e7a24b0343
Toby <5722599@gmail.com>
2023-10-24 17:27:41 -0400

    Added onClick functions for change the slide

--- slider/src/Carousel.jsx
+++ slider/src/Carousel.jsx
@@ -4,15 +4,35 @@ import { FaQuoteRight } from 'react-icons/fa';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 const Carousel = () => {
-  const [customers, setCustomers] = useState(shortList);
+  const [customers, setCustomers] = useState(list);
+  const [currentCustomer, setCurrentCustomer] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentCustomer((previousCustomer) => {
+      const result = (previousCustomer - 1 + customers.length) % customers.length;
+      console.log(result);
+      return result;
+    });
+  };
+
+  const nextSlide = () => {
+    setCurrentCustomer((previousCustomer) => {
+      const result = (previousCustomer + 1) % customers.length;
+      console.log(result);
+      return result;
+    });
+  };
 
   return (
     <div className='main'>
 
-      {customers.map((customer) => {
+      {customers.map((customer, index) => {
         const { id, image, name, title, quote } = customer;
         return (
-          <div className='slider' key={id}>
+          <div className='slider'
+            key={id}
+            style={{ transform: `translateX(${100 * (index - currentCustomer)}%)` }}
+          >
             <img src={image} className='customer-img' alt={name} />
             <h5 className='customer-name'>{name}</h5>
             <p className='customer-title'>{title}</p>
@@ -22,10 +42,10 @@ const Carousel = () => {
         )
       })}
 
-      <button type='button' className='prev-btn'>
+      <button type='button' className='prev-btn' onClick={prevSlide}>
         <FiChevronLeft />
       </button>
-      <button type='button' className='next-btn'>
+      <button type='button' className='next-btn' onClick={nextSlide}>
         <FiChevronRight />
       </button>
     </div>
