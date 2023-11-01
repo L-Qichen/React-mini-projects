fe751098b8a086647608ba6ad074c569bb7966df
Toby <5722599@gmail.com>
2023-11-01 18:01:47 -0400

    rendered items on the page

--- todo-list/src/SingleItem.jsx
+++ todo-list/src/SingleItem.jsx
@@ -1,9 +1,25 @@
+import { useState } from 'react'
+
 const SingleItem = ({ item }) => {
+  const [isChecked, setIsChecked] = useState(false);
 
   return (
-    <div>
-      <div>{item.name}</div>
-    </div>
+    <div className="todo-list-container">
+      <input type="checkbox"
+        className="todo-checkbox"
+        checked={isChecked}
+        onChange={e => setIsChecked(!isChecked)}
+      />
+      <p className="todo-name"
+        style={{
+          textTransform: 'capitalize',
+          textDecoration: isChecked && 'line-through'
+        }}
+      >
+        {item.name}
+      </p>
+      <button type="button" className='btn'>delete</button>
+    </div >
   )
 }
 
