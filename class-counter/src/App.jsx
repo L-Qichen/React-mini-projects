3feda1fe3fc4992104e730b65768c3d377288845
Toby <5722599@gmail.com>
2023-12-01 08:15:57 -0500

    toggle done

--- class-counter/src/App.jsx
+++ class-counter/src/App.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react"
 import ClassContainer from "./ClassCounter"
 import { FunctionCounter } from "./FunctionCounter"
 import Registration from "./Registration"
 import './App.css'
 
 function App() {
+  const [isToggle, setIsToggle] = useState(true);
+
+  const handleToggle = () => {
+    setIsToggle(!isToggle);
+  }
 
   return (
     <>
-      <ClassContainer />
-      <FunctionCounter />
-      <Registration />
+      <button type="button" onClick={handleToggle}
+        style={{
+          display: "block",
+          margin: "2rem auto"
+        }}
+        className="btn">toggle</button >
+      {isToggle ? <></> :
+        <>
+          <ClassContainer />
+          <FunctionCounter />
+          <Registration />
+        </>
+      }
+
     </>
   )
 }
