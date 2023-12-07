97e5daa8de0368d4ad77641a76faa9f913733b9e
Toby <5722599@gmail.com>
2023-12-07 11:20:08 -0500

    Number selection feature done

--- prize-wheel/src/App.jsx
+++ prize-wheel/src/App.jsx
@@ -3,11 +3,27 @@ import './App.css'
 
 function App() {
   const [isStopped, setIsStopped] = useState(true);
+  const [selectedNum, setSelectedNum] = useState([]);
+
+  const selectionRange = [1, 2, 3, 4, 5, 6];
+
+  const handleSelectedNum = (number) => {
+    const newSelectedNum = [...selectedNum];
+    const index = newSelectedNum.indexOf(number);
+
+    if (index === -1) {
+      newSelectedNum.push(number);
+    } else {
+      newSelectedNum.splice(index, 1);
+    }
+    setSelectedNum(newSelectedNum);
+  };
+
 
   const handleOnClick = () => {
     if (isStopped) {
       const randomRounds = Math.floor(Math.random() * (7 - 3) + 3);
-      const randomDeg = Math.floor(Math.random() * (27 - 7) + 7);
+      const randomDeg = Math.floor(Math.random() * (360 - 10) + 10);
       const finalDeg = randomRounds * 360 + randomDeg;
       const wheel = document.getElementById("wheel");
       wheel.style.transform = `rotate(${finalDeg}deg)`;
@@ -19,17 +35,20 @@ function App() {
         wheel.style.transition = "none";
         setIsStopped(true);
       }, 5000);
+
+      console.log('Selected Numbers:', selectedNum);
+      setSelectedNum([]);
     } else {
       return;
     }
   }
 
   return (
-    <>
+    <div className='main'>
       <div className='container'>
         <h1>Prize Wheel</h1>
-        <div className='out-circle' id='wheel'>
-          <div className='in-circle'>
+        <div className='out-circle' >
+          <div className='in-circle' id='wheel'>
 
             <div className='first-container'>
               <div className='content-1'>
@@ -68,14 +87,33 @@ function App() {
 
         <div className='pointer-body'></div>
         <div className='pointer-head'></div>
-        <button type='button'
-          className='btn'
-          onClick={handleOnClick}
-        >
-          Start!
-        </button>
+
+        <div className='footer'>
+          <button type='button'
+            className='btn'
+            onClick={handleOnClick}
+          >
+            Start!
+          </button>
+          <div className="number-list">
+            {[...selectionRange].map(
+              (number) => {
+                return (
+                  <button
+                    key={number}
+                    className={selectedNum.includes(number) ? 'num-btn selected' : 'num-btn'}
+                    onClick={() => handleSelectedNum(number)}
+                  >
+                    {number}
+                  </button>
+                );
+              }
+            )}
+          </div>
+        </div>
       </div>
-    </>
+
+    </div>
   )
 }
 
